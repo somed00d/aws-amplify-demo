38b73fdaa7079d0d74ce73cdd9a6052d9bf3eb0b
somed00d <41293874+somed00d@users.noreply.github.com>
2020-10-13 09:21:33 -0400

    Add imports for Amplify to app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import '@aws-amplify/ui-vue';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';Amplify.configure(awsconfig);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
